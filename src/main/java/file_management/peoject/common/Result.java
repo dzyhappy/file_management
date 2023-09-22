2b45155f10cf405628707e3fac8a0b0ef20a6d7f
dzyhappy <d1421030751@163.com>
2023-09-22 23:27:13 +0800

    返回结果集

---
+++ src/main/java/file_management/peoject/common/Result.java
@@ -0,0 +1,62 @@
+package file_management.peoject.common;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class Result implements Serializable {
+
+    // 1成功，0失败
+    private int code;
+    private String message;
+    private Object data;
+
+    public static Result success() {
+        return Result.success("操作成功", null);
+    }
+
+    public static Result success(Object data) {
+        return Result.success("操作成功", data);
+    }
+
+    /*
+    * {
+          "code": 1,
+          "message": "string",
+          "data": [
+            {
+              "id": 0,
+              "name": "string",
+              "type_id": 0,
+              "overview": "string"
+            }
+          ]
+        }
+        * 成功
+    * */
+    public static Result success(String msg, Object data) {
+        Result result = new Result();
+        result.code = 1;
+        result.message = msg;
+        result.data = data;
+        return result;
+    }
+
+    /*
+    * {
+          "code": 0,
+          "message": "string",
+          "data": "Unknown Type: null"
+       }
+       * 失败
+    * */
+    public static Result fail(String msg) {
+        Result result = new Result();
+        result.code = 0;
+        result.message = msg;
+        result.data = null;
+        return result;
+    }
+
+}
